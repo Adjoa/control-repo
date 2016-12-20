6d0237883fa9e0d960a7938d01f7ae16a5e1ec23
Adjoa Darien <adjoa.darien@qa.com>
2016-12-20 13:34:11 -0500

    added hiera parameters to website.pp

--- site/profile/manifests/website.pp
+++ site/profile/manifests/website.pp
@@ -1,10 +1,21 @@
 class profile::website (
-  $vhosts = hiera('vhosts')
+  $default_vhosts = hiera('apache::vhosts'),
+  $vhosts         = hiera('profile::website::vhosts')
 ){
   class { 'apache':
-    default_vhost => false,
+    default_vhost => $default_vhosts,
   } ->
   class { 'apache::vhosts':
     vhosts => $vhosts,
+  }
+
+  $vhosts.each |Hash $vhost| {
+    $vhost_docroot = $vhost['docroot']
+
+    file { "${vhost_docroot}/default.html":
+      ensure  => file,
+      content => "Hello, world!",
+      require => Class["apache::vhosts"],
+    } 
   }
 }
