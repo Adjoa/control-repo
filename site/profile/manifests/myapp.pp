879f5bd751febb294a6e6a5171e6ce013357df83
Adjoa Darien <adjoa.darien@qa.com>
2017-01-24 13:35:51 +0000

    added chain to myapp.pp resources

--- site/profile/manifests/myapp.pp
+++ site/profile/manifests/myapp.pp
@@ -10,18 +10,15 @@ class profile::myapp (
 
   tomcat::instance { 'default':
     catalina_home => '/opt/tomcat',
-  }
-
+  } ->
   file {'myapp war source':
     ensure => file,
     name   => "/opt/tomcat/webapps/${app_name}.war",
     source => "puppet:///modules/profile/webapps/${app_name}.war",
-  }
-
+  } ->
   tomcat::war { 'sample.war':
     catalina_base => '/opt/tomcat',
     war_source    => "/opt/tomcat/webapps/${app_name}.war",
-    require       => File['myapp war source'],
   }   
 
   firewall { '101 allow access to myapp':
